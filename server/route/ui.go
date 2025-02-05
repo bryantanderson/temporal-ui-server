7b964ad75c5ecdf4af067181b21eb45a9cdb38b7
Bryant <andersonciputra@gmail.com>
2025-02-05 15:05:45 +1100

    remove CSP

--- server/route/ui.go
+++ server/route/ui.go
@@ -24,12 +24,9 @@ package route
 
 import (
 	"bytes"
-	"crypto/rand"
-	"encoding/hex"
 	"fmt"
 	"html/template"
 	"io/fs"
-	"log"
 	"net/http"
 	"path"
 	"regexp"
@@ -73,17 +70,23 @@ func removeCSPMeta(htmlStr string) string {
 
 func buildUIIndexHandler(publicPath string, assets fs.FS) (echo.HandlerFunc, error) {
 	indexHTMLBytes, err := fs.ReadFile(assets, "index.html")
+
 	if err != nil {
 		return nil, err
 	}
+
+	// Won't be executed
 	if publicPath != "" {
 		indexHTML := string(indexHTMLBytes)
 		indexHTML = strings.ReplaceAll(indexHTML, "base: \"\"", fmt.Sprintf("base: \"%s\"", publicPath))
 		indexHTML = strings.ReplaceAll(indexHTML, "\"/_app/", fmt.Sprintf("\"%s/_app/", publicPath))
-		log.Printf("WARNING: CSP IS DISABLED WHEN USING PUBLIC PATH!")
-		indexHTML = removeCSPMeta(indexHTML)
 		indexHTMLBytes = []byte(indexHTML)
 	}
+
+	// Remove CSP meta tag
+	indexHTML := string(indexHTMLBytes)
+	indexHTML = removeCSPMeta(indexHTML)
+	indexHTMLBytes = []byte(indexHTML)
 
 	return func(c echo.Context) (err error) {
 		return c.Stream(200, "text/html", bytes.NewBuffer(indexHTMLBytes))
@@ -93,23 +96,6 @@ func buildUIIndexHandler(publicPath string, assets fs.FS) (echo.HandlerFunc, err
 func buildUIAssetsHandler(assets fs.FS) echo.HandlerFunc {
 	handler := http.FileServer(http.FS(assets))
 	return echo.WrapHandler(handler)
-}
-
-func generateNonce() string {
-	bytes := make([]byte, 16)
-	if _, err := rand.Read(bytes); err != nil {
-		panic(err)
-	}
-	return hex.EncodeToString(bytes)
-}
-
-// Generate CSP header
-func generateCSP(nonce string) string {
-	return fmt.Sprintf(
-		"base-uri 'self'; default-src 'none'; style-src 'nonce-%s'; script-src 'nonce-%s'; frame-ancestors 'self'; form-action 'none'; sandbox allow-same-origin allow-popups allow-popups-to-escape-sandbox;",
-		nonce,
-		nonce,
-	)
 }
 
 // Process markdown content
@@ -140,7 +126,7 @@ const pageTemplate = `
 	<base target="_blank">
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<style nonce="{{.Nonce}}">
+	<style>
 			{{.CSS}}
 	</style>
 </head>
@@ -165,8 +151,6 @@ func SetRenderRoute(e *echo.Echo, publicPath string) {
 		// Process markdown to HTML
 		renderedHTML := processMarkdown(content)
 
-		nonce := generateNonce()
-
 		data := struct {
 			Content template.HTML
 			Nonce   string
@@ -174,13 +158,11 @@ func SetRenderRoute(e *echo.Echo, publicPath string) {
 			CSS     string
 		}{
 			Content: template.HTML(renderedHTML),
-			Nonce:   nonce,
 			Theme:   theme,
 		}
 
 		// Set headers
 		c.Response().Header().Set("Content-Type", "text/html")
-		c.Response().Header().Set("Content-Security-Policy", generateCSP(nonce))
 
 		err := tmpl.Execute(c.Response().Writer, data)
 		if err != nil {
